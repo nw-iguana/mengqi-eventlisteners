91d6e27c0268d4afd9b23edfc469b51bfb8debd3
Mengqi Wang <wmq516@gmail.com>
2019-02-01 22:53:31 -0700

    added initial work

--- index.js
+++ index.js
@@ -6,3 +6,29 @@ $(function() {
       $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
   })
 })
+
+// Below is what we have got initially
+function fizzBuzz() {
+  $('#number-chooser').submit(function(event) {
+    event.preventDefault();
+    let count = $('#number-choice').val();
+    let result = [];
+    for (let i = 0; i < count; i++) {
+      if ((i % 3 !== 0) && (i % 5 !== 0)) {
+        result.push(`
+         <div class="fizz-buzz-item">
+        <span>${i}</span>
+        </div>
+      `);
+      }
+      
+      }
+      console.log(result);
+    $('.js-results').append(result);
+    $('#number-choice').val(" ");  
+  } )
+  
+  
+  }
+  
+  $(fizzBuzz);
