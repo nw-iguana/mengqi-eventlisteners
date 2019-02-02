d8cec09f3023cf1efa6f603b98c1052559316d6f
Mengqi Wang <wmq516@gmail.com>
2019-02-01 22:59:56 -0700

    added comment

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//We did not target the img tag initially
+//Sorry I no longer have our initial version by the end of class
+
 $(function() {
  $('.thumbnail').on('click', function(event) {
     let imgSrc = $(this).find('img').attr('src');  
@@ -6,29 +9,3 @@ $(function() {
       $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
   })
 })
-
-// Below is what we have got initially
-function fizzBuzz() {
-  $('#number-chooser').submit(function(event) {
-    event.preventDefault();
-    let count = $('#number-choice').val();
-    let result = [];
-    for (let i = 0; i < count; i++) {
-      if ((i % 3 !== 0) && (i % 5 !== 0)) {
-        result.push(`
-         <div class="fizz-buzz-item">
-        <span>${i}</span>
-        </div>
-      `);
-      }
-      
-      }
-      console.log(result);
-    $('.js-results').append(result);
-    $('#number-choice').val(" ");  
-  } )
-  
-  
-  }
-  
-  $(fizzBuzz);
